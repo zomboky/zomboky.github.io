40dd66baad77d123c6115662b10bebfd53903364
zomboky <remi.tournie@gmail.com>
2025-08-14 21:04:41 +0200

    première grosse update ajout de three.js

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,5 @@
-import * as THREE from '../js/three.module.min.js';
+import * as THREE from '../three/build/three.module.min.js';
+import {OrbitControls} from '../three/examples/jsm/controls/OrbitControls.js';
 
 console.log("le script test.js a bien été chargé");
 
@@ -27,26 +28,89 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 camera.position.setZ(30);
 
-renderer.render( scene, camera); 
+// renderer.render( scene, camera); 
+
+
+//const geometry = new THREE.SphereGeometry( 15, 32, 16 ); 
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material_basic = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe : true } ); 
+const material_standard = new THREE.MeshStandardMaterial( { color : 0xFF6347 });
+const object = new THREE.Mesh(geometry, material_standard);
+
+scene.add(object);
+
+
+// Contrôle souris
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Lumières 
+
+const pointlight = new THREE.PointLight(0xffffff);
+pointlight.position.set(0, 0, 0);
+
+const ambientlight = new THREE.AmbientLight(0xffffff);
+ambientlight.position.set()
+
+scene.add(pointlight, ambientlight); // le code parle de lui même c'est golmon
+
+// helpers 
+
+const lighthelper = new THREE.PointLightHelper(pointlight);
+const gridhelper = new THREE.GridHelper(200, 50);
+//scene.add(lighthelper, gridhelper);
+
+// Add stars
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+
+// Move Camera 
+
+function MoveCamera(){
+    const t = document.body.getBoundingClientRect().top;
+
+    camera.position.z = t * -0.001;
+    camera.position.x = t * -0.01;
+    camera.position.y = t * -0.002;
 
 
 
-const geometry = new THREE.SphereGeometry( 15, 32, 16 ); 
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
-const sphere = new THREE.Mesh(geometry, material);
+}
 
-scene.add(sphere);
+document.body.onscroll = MoveCamera;
+MoveCamera();
 
 function animate(){
 
+
     requestAnimationFrame( animate );
 
-    sphere.rotation.x += 0.01; 
+    object.rotation.x += 0.01; 
+    object.rotation.y += 0.005;
+    object.rotation.z += 0.01;
+
+    controls.update();
 
     renderer.render( scene, camera);
 
 }
 
 animate();
+
+
 
 
